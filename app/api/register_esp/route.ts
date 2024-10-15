c6f6d3e5d52ea19be4be7ce8c1381c67433f1434
Liam Kosar <liamkosar@Liams-MacBook-Pro.local>
2024-10-14 22:40:45 -0400

    Fix: failing api if bad key

--- app/api/register_esp/route.ts
+++ app/api/register_esp/route.ts
@@ -4,7 +4,10 @@ const arduino_api_key_check = process.env.ARDUINO_API_KEY;
 
 // To handle a GET request to /api
 export async function GET(request: Request) {
-  return NextResponse.json({ message: "No get available at this endpoint." }, { status: 400 });
+  return NextResponse.json(
+    { message: "No get available at this endpoint." },
+    { status: 400 }
+  );
 }
 
 // To handle a POST request to /api
@@ -19,13 +22,21 @@ export async function POST(request: Request) {
 
     if (authorization === arduino_api_key_check) {
       console.log("woooohfoashdfljahsdf");
+      // Log the headers
+      console.log("Content-Type:", contentType);
+      console.log("Authorization Header:", authorization);
+      // Do whatever you want with the parsed body
+      return NextResponse.json(
+        { message: "Api key correct", data: body },
+        { status: 200 }
+      );
     }
-
-    // Log the headers
-    console.log("Content-Type:", contentType);
-    console.log("Authorization Header:", authorization);
-    // Do whatever you want with the parsed body
-    return NextResponse.json({ message: "Data received", data: body }, { status: 200 });
+    else {
+      return NextResponse.json(
+        { message: "Api key incorrect", data: body },
+        { status: 400 }
+      );
+    }
   } catch (error) {
     console.error("Error reading request body:", error);
     return NextResponse.json({ message: "Invalid JSON body" }, { status: 400 });

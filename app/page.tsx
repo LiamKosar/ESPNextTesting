9eb398836ce9724634879c6fdf6e38f08b98f113
Liam Kosar <liamkosar@Liams-MacBook-Pro.local>
2024-10-16 00:22:21 -0400

    fix: brizz

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,6 @@ import { useUser } from '@auth0/nextjs-auth0/client';
 export default function Home() {
   const { user, error, isLoading } = useUser();
   return <main>
-    {user ? <h1>{user.email}</h1> : <h1>home</h1> }
+    {user ? <h1>{user.email} Give me back my meat sauce</h1> : <h1>fasdf</h1> }
   </main>;
 }
